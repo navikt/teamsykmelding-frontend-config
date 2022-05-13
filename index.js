a680fee355a76f6ba06b9391a53bbfd2c9221f72
Karl J. Overå <k@rl.run>
2022-05-13 17:03:29 +0200

    rename and add eslint

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["prettier", "testing-library", "postcss-modules"],
+  rules: {
+    "prettier/prettier": "warn",
+    "import/order": ["warn", { "newlines-between": "always" }],
+    "react-hooks/exhaustive-deps": [
+      "warn",
+      { additionalHooks: "(useUpdateBreadcrumbs)" },
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true },
+    ],
+    "postcss-modules/no-undef-class": "error",
+    "postcss-modules/no-unused-class": "warn",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+      rules: {
+        "testing-library/no-debugging-utils": "warn",
+      },
+    },
+  ],
+  settings: {
+    "postcss-modules": {
+      exclude: ["**/node_modules/**/*", "**/global.css"],
+    },
+  },
+};
